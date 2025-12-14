63c55a8436b6f463344f278207cc1be001d047cf
g-ae <goncalo.arieira@hotmail.com>
2025-12-14 13:06:08 +0100

    feat: added buyer account register

--- backend/src/controllers/account_buyer.controller.js
+++ backend/src/controllers/account_buyer.controller.js
@@ -39,8 +39,25 @@ const buyerRegister = (req, res) => {
   // pas de vérification par mail/téléphone pour l'instant car trop compliqué pour ce mini-projet
   // retour d'un web token aussi
   
-  rounds = 10 // this number can be changed later as the number of rounds is available in the hash
+  const rounds = 10 // this number can be changed later as the number of rounds is available in the hash
   
+  const {email, password, name, telephone} = req.body
+  // TODO: add verification that email is only used once
+  
+  if (!email || !password || !name || !telephone) {
+    return res.status(400).json({message: "missing arguments"})
+  }
+  
+  const hashPw = bcrypt.hashSync(password, rounds)
+  
+  pool.query("INSERT INTO buyer(email, password, name, telephone) VALUES ($1, $2, $3, $4)", [email, hashPw, name, telephone], (error, results) => {
+    if (error) {
+      console.log(error)
+      return res.status(502).json({message: "db error"})
+    }
+    
+    return res.status(200).json()
+  })
 }
 
 export {
